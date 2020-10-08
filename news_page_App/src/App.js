eacb6af22043d42ccaa673210cdd3f2b31e0ba75
huseyin <hbayrak.hbayrak@gmail.com>
2020-10-08 08:53:44 +0300

    1.phase

---
+++ news_page_App/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+      <Text>selam</Text>
+    </View>
+  );
+};
+
+export default App;
